9fe82a05d77ad6687a79c0e8a2e494a0c226067d
Ali Pirpiran <alipirpiran@gmail.com>
2020-03-24 21:24:41 +0430

    add getCoin by its symbol

--- api/binance.js
+++ api/binance.js
@@ -13,3 +13,9 @@ var getETH = function() {
         'https://www.binance.com/api/v3/avgPrice?symbol=ETHUSDT'
     );
 };
+
+var getCoin = function(symbol){
+    return request.get(
+        `https://www.binance.com/api/v3/avgPrice?symbol=${symbol}`
+    );
+}
