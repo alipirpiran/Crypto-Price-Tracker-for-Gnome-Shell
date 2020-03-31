cbcd1c29299448c209a533031004d3d5c752c553
Ali Pirpiran <alipirpiran@gmail.com>
2020-03-31 13:10:11 +0430

    create coinitem2, rebuilding coinitem

--- models/coinItem.js
+++ models/coinItem.js
@@ -8,6 +8,12 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
 const Binance = Me.imports.api.binance;
+const Settings = Me.imports.settings;
+const convenience = Me.imports.convenience;
+
+const Schema = convenience.getSettings(
+    'org.gnome.shell.extensions.crypto-tracker'
+);
 
 const PopupMenu = imports.ui.popupMenu;
 var CoinItem = GObject.registerClass(
@@ -38,7 +44,7 @@ var CoinItem = GObject.registerClass(
 
         _startTimer() {
             let menuItem = Me.imports.extension.menuItem;
-        
+
             this._refreshPrice(menuItem);
 
             this.timeOutTag = GLib.timeout_add(1, 1000 * 10, async () => {
@@ -85,6 +91,118 @@ var CoinItem = GObject.registerClass(
                     coin.activeCoin = false;
                 }
             }
+        }
+    }
+);
+
+var CoinItem2 = GObject.registerClass(
+    {
+        Signals: { toggled: { param_types: [GObject.TYPE_BOOLEAN] } },
+    },
+    class CoinItem2 extends PopupMenu.PopupBaseMenuItem {
+        _init(symbol, text, active) {
+            super._init();
+            this.add_style_class_name('coinItem popup-submenu-menu-item');
+
+            let icon = new St.Icon({
+                icon_name: 'edit-delete-symbolic',
+                style_class: 'popup-menu-icon',
+            });
+            let delBtn = new St.Button({
+                child: icon,
+                style_class: 'btn m0',
+            });
+            delBtn.connect('clicked', this._delCoin.bind(this));
+            this.add_child(delBtn);
+
+            let expander = new St.Bin({
+                x_expand: true,
+            });
+            this.add_child(expander);
+
+            this.label = new St.Label({
+                text,
+                y_align: Clutter.ActorAlign.CENTER,
+                x_align: Clutter.ActorAlign.START,
+                x_expand: true,
+            });
+            this.add_child(this.label);
+
+            this.text = text;
+            this.symbol = symbol;
+            this.activeCoin = active;
+            this.timeOutTage;
+
+            if (active) this._activeCoin();
+            this._startTimer();
+
+            this.connect('toggled', this.toggleCoin.bind(this));
+            Me.imports.extension.addCoin(this);
+        }
+        _activeCoin() {
+            let menuItem = Me.imports.extension.menuItem;
+
+            menuItem.text = this.text + ' ...';
+            this.activeCoin = true;
+        }
+        _getPrice() {
+            return Binance.getCoin(this.symbol);
+        }
+
+        _startTimer() {
+            let menuItem = Me.imports.extension.menuItem;
+
+            this._refreshPrice(menuItem);
+
+            this.timeOutTag = GLib.timeout_add(1, 1000 * 10, async () => {
+                this._refreshPrice(menuItem);
+                return true;
+            });
+        }
+        async _refreshPrice(menuItem) {
+            let result = await this._getPrice();
+            const jsonRes = JSON.parse(result.body);
+            let price = jsonRes.price;
+            let priceParts = price.split('.');
+
+            const totalLen = 6;
+            let len = 0;
+            len += priceParts[0].length;
+            price = priceParts[0] + '.';
+            let i = 0;
+            for (len; len < totalLen; len++) {
+                price += priceParts[1][i];
+                i++;
+            }
+
+            if (this.activeCoin) menuItem.text = `${this.text} ${price}`;
+            this.label.text = `${this.text}   ${price}     `;
+        }
+
+        toggleCoin() {
+            if (this.state) {
+                this._activeCoin.bind(this)();
+                this.disableOtherCoins();
+            }
+        }
+
+        removeTimer() {
+            if (this.timeOutTag) GLib.Source.remove(this.timeOutTag);
+        }
+
+        disableOtherCoins() {
+            for (const coin of Me.imports.extension.indicator.coins) {
+                if (coin == this) continue;
+                if (coin.state) {
+                    coin.toggle();
+                    coin.activeCoin = false;
+                }
+            }
+        }
+
+        _delCoin() {
+            Settings.delCoin(Schema, { name: this.text });
+            Me.imports.extension.indicator.removeCoinFromMenu(this);
         }
     }
 );
