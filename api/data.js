b2ecf3aac13f75f90cef8d81ea59704a416e460a
Ali Pirpiran <alipirpiran@gmail.com>
2022-03-28 20:48:42 +0430

    exchange support get price from selected exchange API

--- api/data.js
+++ api/data.js
@@ -1,35 +1,61 @@
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
+const Settings = Me.imports.settings;
 
 const request = Me.imports.api.request;
+let current_exchange = '';
 
-var getPrice = async function (symbol) {
-	const url = 'https://www.okx.com/api/v5/market/ticker?instId=';
-	const res = await request.get(url + symbol);
-
-	const jsonRes = JSON.parse(res.body);
-
-	if (jsonRes.data.length == 0) return -1;
-
-	let price = +jsonRes.data[0].last;
-
-	return price.toLocaleString();
-
-	let priceParts = price.split('.');
-
-	const totalLen = 6;
-	let len = 0;
-	len += priceParts[0].length;
-	price = priceParts[0] + '.';
-	let i = 0;
-	for (len; len < totalLen; len++) {
-		price += priceParts[1][i];
-		i++;
-	}
-	if (+price == 0)
-		for (let i = len; i < priceParts[1].length; i++) {
-			price += priceParts[1][i];
-		}
-
-	return price;
+const exchanges = {
+  binance: 'Binance',
+  okx: 'OKX',
 };
+
+var change_exchange = (exchange_name) => {
+  current_exchange = exchange_name;
+  Settings.change_exchange(exchange_name);
+};
+
+var get_exchange = () => {
+  if (current_exchange != '') return current_exchange;
+  current_exchange = Settings.get_exchange();
+  return current_exchange;
+};
+
+var getPrice = async function (name, vol) {
+  switch (get_exchange()) {
+    case exchanges.binance:
+      return _getPriceFromBinance(name, vol);
+
+    case exchanges.okx:
+      return _getPriceFromOKX(name, vol);
+  }
+};
+
+async function _getPriceFromBinance(name, vol) {
+  const url = 'https://api.binance.com/api/v3/ticker/price?symbol=';
+  const res = await request.get(url + name + vol);
+
+  const jsonRes = JSON.parse(res.body);
+  if (jsonRes.code) return jsonRes.msg.slice(0, 30) + '...';
+
+  let price = +jsonRes.price;
+
+  let maximumFractionDigits = 0;
+  const dig_count = price.toFixed().length;
+  if (5 - dig_count > 0) maximumFractionDigits = 5 - dig_count;
+
+  return price.toLocaleString(undefined, { maximumFractionDigits });
+}
+
+async function _getPriceFromOKX(name, vol) {
+  const url = 'https://www.okx.com/api/v5/market/ticker?instId=';
+  const res = await request.get(url + name + '-' + vol);
+
+  const jsonRes = JSON.parse(res.body);
+
+  if (jsonRes.data.length == 0) return -1;
+
+  let price = +jsonRes.data[0].last;
+
+  return price.toLocaleString();
+}
