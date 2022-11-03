1be18f564ae04f73babd066e27e8e96f963da596
Ali Pirpiran <alipirpiran@gmail.com>
2022-11-03 18:38:38 +0330

    add support soup v3

--- api/request.js
+++ api/request.js
@@ -2,70 +2,61 @@ const Soup = imports.gi.Soup;
 let _soupASyncSession;
 
 function _getSession() {
-  if (!_soupASyncSession) _soupASyncSession = new Soup.SessionAsync();
+  if (!_soupASyncSession) _soupASyncSession = new Soup.Session();
   return _soupASyncSession;
 }
 
 function get(url) {
+  switch (Soup.MAJOR_VERSION) {
+    case 2:
+      return get_soup_v2(url);
+    case 3:
+      return get_soup_v3(url);
+  }
+}
+
+function get_soup_v3(url) {
   return new Promise((resolve, reject) => {
-    var _httpSession = new Soup.SessionAsync();
+    var session = new Soup.Session();
+
+    var message = Soup.Message.new('GET', url);
+
+    session.send_and_read_async(
+      message,
+      GLib.PRIORITY_DEFAULT,
+      null,
+      function (session, result) {
+        if (message.get_status() === Soup.Status.OK) {
+          let bytes = session.send_and_read_finish(result);
+          let decoder = new TextDecoder('utf-8');
+          let response = decoder.decode(bytes.get_data());
+
+          resolve({
+            code: result.status_code,
+            body: response,
+          });
+        }
+      }
+    );
+  });
+}
+
+function get_soup_v2(url) {
+  return new Promise((resolve, reject) => {
+    var session = new Soup.SessionAsync();
     Soup.Session.prototype.add_feature.call(
-      _httpSession,
+      session,
       new Soup.ProxyResolverDefault()
     );
 
-    var request = Soup.Message.new('GET', url);
+    var message = Soup.Message.new('GET', url);
 
-    _httpSession.queue_message(request, function (_httpSession, message) {
+    session.queue_message(message, function (_httpSession, result) {
       // request is done
       resolve({
-        code: message.status_code,
-        body: request.response_body.data,
+        code: result.status_code,
+        body: message.response_body.data,
       });
     });
-  });
-}
-
-function get2(url) {
-  return new Promise((resolve, reject) => {
-    try {
-      var _httpSession = new Soup.SessionAsync();
-
-      Soup.Session.prototype.add_feature.call(
-        _httpSession,
-        new Soup.ProxyResolverDefault()
-      );
-
-      var request = Soup.Message.c_new('GET', url);
-
-      _httpSession.queue_message(request, function (_httpSession, message) {
-        // request is done
-        resolve({
-          code: message.status_code,
-          body: request.response_body.data,
-        });
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
-
-function get3(url) {
-  return new Promise((resolve, reject) => {
-    try {
-      let request = Soup.Message.new('GET', url);
-      const _httpSession = _getSession();
-      _httpSession.queue_message(request, function (_httpSession, message) {
-        // request is done
-        resolve({
-          code: message.status_code,
-          body: request.response_body.data,
-        });
-      });
-    } catch (error) {
-      // print(error);
-      reject(error);
-    }
   });
 }
