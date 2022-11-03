20cbf9b861a5b33d01105b245b2691b04b242a71
azorpax <azorpax@gmail.com>
2022-11-04 03:17:15 +0800

    add button for opening chart and some changes for toggling

--- models/coinItem.js
+++ models/coinItem.js
@@ -1,9 +1,5 @@
-const GLib = imports.gi.GLib;
-const GObject = imports.gi.GObject;
-const St = imports.gi.St;
-const Clutter = imports.gi.Clutter;
-const Signals = imports.signals;
-const Atk = imports.gi.Atk;
+// noinspection DuplicatedCode
+const { Atk, Clutter, Gtk, GLib, GObject, St } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
@@ -31,6 +27,46 @@ var CoinItem = GObject.registerClass(
       this.accessible_role = Atk.Role.CHECK_MENU_ITEM;
       this.checkAccessibleState();
 
+      let viewIcon = new St.Icon({
+        icon_name: 'external-link-symbolic',
+        style_class: 'popup-menu-icon',
+      });
+      let viewBtn = new St.Button({
+        child: viewIcon,
+        style_class: 'btn m0',
+      });
+      viewBtn.connect('clicked', this._openChart.bind(this));
+      this.add_child(viewBtn);
+
+      this.nameLbl = new St.Label({
+        text: title || symbol,
+        style_class: "itemLabel",
+        y_align: Clutter.ActorAlign.CENTER,
+      });
+      this.add_child(this.nameLbl);
+
+      this.priceLbl = new St.Label({
+        text: "...",
+        style_class: 'itemLabel text-align-right',
+        y_align: Clutter.ActorAlign.CENTER,
+      });
+      this.add_child(this.priceLbl);
+
+      let expander = new St.Bin({
+        style_class: 'popup-menu-item-expander',
+        x_expand: true,
+      });
+      this.add_child(expander);
+
+      this._statusBtn = new St.Button({
+        x_align: Clutter.ActorAlign.START,
+        // x_expand: true,
+        child: this._switch
+      });
+      this._statusBtn.connect('clicked', this._toggle.bind(this, menuItem));
+      this.add_child(this._statusBtn);
+      // this._statusBin.child = this._switch;
+
       let icon = new St.Icon({
         icon_name: 'edit-delete-symbolic',
         style_class: 'popup-menu-icon',
@@ -42,27 +78,6 @@ var CoinItem = GObject.registerClass(
       delBtn.connect('clicked', this._delCoin.bind(this));
       this.add_child(delBtn);
 
-      this.label = new St.Label({
-        text: title || symbol,
-        y_expand: true,
-        y_align: Clutter.ActorAlign.CENTER,
-      });
-      this.add_child(this.label);
-
-      let expander = new St.Bin({
-        style_class: 'popup-menu-item-expander',
-        x_expand: true,
-      });
-      this.add_child(expander);
-
-      this._statusBin = new St.Bin({
-        // x_align: Clutter.ActorAlign.END,
-        // x_expand: true,
-      });
-      this.add_child(this._statusBin);
-      this._statusBin.child = this._switch;
-
-      // this.text = text;
       this.symbol = symbol;
       this.activeCoin = active;
       this.title = title;
@@ -71,8 +86,6 @@ var CoinItem = GObject.registerClass(
 
       if (active) this._activeCoin(menuItem, true);
       this._startTimer(menuItem);
-
-      this.connect('toggled', this.toggleCoin.bind(this, menuItem));
     }
     _activeCoin(menuItem, isInit) {
       this.activeCoin = true;
@@ -120,12 +133,12 @@ var CoinItem = GObject.registerClass(
               ') ((\\.\\.\\.)|(\\d*(,?\\d\\d\\d)*|\\d+)(\\.?\\d*))?',
             'g'
           );
-          menuItem.text = menuItem.text.replace(
-            re,
-            `${this.title || this.symbol} ${price}`
+          let menuPairPrice = isNaN(price.replaceAll(",","")) ? "..." : price;
+          menuItem.text = menuItem.text.replace(re, `${this.title || this.symbol} ${menuPairPrice}`
           );
         }
-        this.label.text = `${this.title || this.symbol}    ${price}     `;
+        this.nameLbl.text = `${this.title || this.symbol}`;
+        this.priceLbl.text = `${price}`;
       } catch (error) {}
     }
     get state() {
@@ -137,22 +150,22 @@ var CoinItem = GObject.registerClass(
     }
 
     activate(event) {
-      if (this._switch.mapped) this.toggle();
+      if (this._switch.mapped) return;
 
       // we allow pressing space to toggle the switch
       // without closing the menu
       if (
-        event.type() == Clutter.EventType.KEY_PRESS &&
-        event.get_key_symbol() == Clutter.KEY_space
+        event.type() === Clutter.EventType.KEY_PRESS &&
+        event.get_key_symbol() === Clutter.KEY_space
       )
         return;
 
       super.activate(event);
     }
 
-    toggle() {
+    _toggle(menu) {
       this._switch.toggle();
-      this.emit('toggled', this._switch.state);
+      this.toggleCoin(menu);
       this.checkAccessibleState();
     }
 
@@ -181,8 +194,9 @@ var CoinItem = GObject.registerClass(
     }
 
     _updateMenuCoinItems(menuItem, isInit) {
-      let newMenuItemText = this.coins
-        .filter(({ activeCoin }) => activeCoin)
+      let activeCoins = this.coins
+          .filter(({ activeCoin }) => activeCoin);
+      let newMenuItemText = activeCoins
         .map(({ title, symbol }) => `${title || symbol} ...`)
         .join(' | ');
 
@@ -191,11 +205,27 @@ var CoinItem = GObject.registerClass(
           (newMenuItemText ? ' | ' : '') + `${this.title || this.symbol} ...`;
 
       menuItem.text = newMenuItemText || '₿';
+
+      if (!isInit)
+        activeCoins.forEach((coin) => coin._refreshPrice(menuItem));
     }
 
     _delCoin() {
       Settings.delCoin({ symbol: this.symbol });
       this.destroy();
+    }
+
+    _openChart() {
+      try {
+        let exchangeUrl = Data.get_exchange() === Data.exchanges.okx ? "https://www.okx.com/markets/spot-info/" : "https://www.binance.com/en/trade/";
+        let pair = Data.get_exchange() === Data.exchanges.okx ? this.symbol.replace('/', '-').toLowerCase() : this.symbol.replace('/', '_').toUpperCase();
+
+        Gtk.show_uri(null, exchangeUrl + pair, global.get_current_time());
+      }
+      catch (err) {
+        let title = _("Can not open %s").format(url);
+        Main.notifyError(title, err);
+      }
     }
 
     destroy() {
