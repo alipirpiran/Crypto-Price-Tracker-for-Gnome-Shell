c6ee678e78747f401a355c1b2b13e9697daaae79
Ali Pirpiran <alipirpiran@gmail.com>
2022-11-10 12:49:21 +0330

    pack script

---
+++ pack.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+gnome-extensions pack -f \
+  --extra-source=api \
+  --extra-source=assets \
+  --extra-source=models \
+  --extra-source=utils \
+  --extra-source=settings.js \
+  --extra-source=stylesheet.css \
+  --extra-source=LICENSE \
+  --extra-source=README.md
